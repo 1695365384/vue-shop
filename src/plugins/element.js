b079eed44e00d8181e7c6c6468b139d7a6202ae7
1695365384 <1695365384@qq.com>
2019-06-26 23:08:28 +0800

    头部完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,25 +4,25 @@ import "element-ui/lib/theme-chalk/index.css";
 
 Vue.use(Element);
 
-import upperFirst from 'lodash/upperFirst'
-import camelCase from 'lodash/camelCase'
-const requireComponent = require.context(
-    // 其组件目录的相对路径
-    'element-ui/lib',
-    // 是否查询其子目录
-    false, 正则)
-// 匹配基础组件文件名的正则表达式
-requireComponent.keys().forEach(fileName => {
-    // 获取组件配置
-    const componentConfig = requireComponent(fileName)
-    // 获取组件的 PascalCase 命名
-    const componentName = upperFirst(
-        camelCase(
-            // 获取和目录深度无关的文件名
-            fileName
-                .split('/')
-                .pop()
-                .replace(正则)
-        )
-    )
-})
+// import upperFirst from 'lodash/upperFirst'
+// import camelCase from 'lodash/camelCase'
+// const requireComponent = require.context(
+//     // 其组件目录的相对路径
+//     'element-ui/lib',
+//     // 是否查询其子目录
+//     false, 正则)
+// // 匹配基础组件文件名的正则表达式
+// requireComponent.keys().forEach(fileName => {
+//     // 获取组件配置
+//     const componentConfig = requireComponent(fileName)
+//     // 获取组件的 PascalCase 命名
+//     const componentName = upperFirst(
+//         camelCase(
+//             // 获取和目录深度无关的文件名
+//             fileName
+//                 .split('/')
+//                 .pop()
+//                 .replace(正则)
+//         )
+//     )
+// })
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <div class="navbar">
+      <div class="w clearfix">
+        <el-button type="info" class="fr">退出</el-button>
+        <a href="/" class="fl">
+          <img src="../assets/logo.png">
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+<style lang="less" scoped>
+.navbar {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.fr {
+  margin-top: 8px;
+}
+</style>
 
 <script>
 export default {
   name: 'login',
   data() {
     return {}
+  },
+  methods: {
+    handleSelect() {}
   }
 }
 </script>
